b889003b4f4afc3e89f5a818317afa11e89c54f2
Jake DeSantis <jake.desantis@gmail.com>
2019-08-20 21:39:49 -0400

    Add files via upload

---
+++ hw.f90
@@ -0,0 +1,18 @@
+program hw
+
+! Jake's first FORTRAN program
+   implicit none
+
+! Type declarations
+   real :: a, b, result
+
+! Executable statements
+   print *, 'Hello world!'
+   a = 1.0
+   b = 2.0
+   result = a + b
+   print *, '1 + 2 is  ', result
+
+end program hw
+
+
